51fcf5a0122407300b761e3eacfbd2c2679fce13
2k24it12414004-cloud <2k24it12414004@gmail.com>
2026-09-01 14:40:30 +0530

    Time: 0 ms (100%), Space: 10.4 MB (96.02%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//by me 
     string frequencySort(string s) {
         map<char,int>mp;
         for(auto x:s){
@@ -16,10 +17,12 @@ public:
       //  string c="";
       int id=0;
         while(pq.size()>0){
+            //p store frequency
           int  p=pq.top().first;
-          for(int i=0;i<p;i++){
+          for(int i=0;i<p;i++){//fill each element jab tak uska frequency ho tab tak
             s[id]=pq.top().second;
-            id++;
+            //store character value
+            id++;//always icrease index saves from memory limit exceeded
           }
 
             pq.pop();
