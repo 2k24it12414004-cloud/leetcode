1ea5f331f9dc1194a5bdbc6ad1c0054954aa7258
2k24it12414004-cloud <2k24it12414004@gmail.com>
2026-09-08 15:01:54 +0530

    Time: 4 ms (68.94%), Space: 15.6 MB (46.3%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -8,27 +8,27 @@ public:
         if(colors.size()<3){
             return false;
         }
-        for(int i=0;i<colors.size();i++){
-            if(i>0&&i<n-1){
+        for(int i=1;i<colors.size()-1;i++){
+            // if(i>0&&i<n-1){
                 if(colors[i]=='A'){
                     if(colors[i-1]=='A'&&colors[i+1]=='A'){
-                        colors[i]=='G';
-                         flag=true;
+                        // colors[i]=='G';
+                        //  flag=true;
                          c1++;
                     }
                     
                 }
                 else if(colors[i]=='B'){
                     if(colors[i-1]=='B'&&colors[i+1]=='B'){
-                        colors[i]=='G';
-                         flag=false;
+                        // colors[i]=='G';
+                        //  flag=false; no need
                          c2++;
                     }
-                }
+                // }
             }
         }
         
-        if(c1!=0&&c1>c2)
+        if(c1!=0&&c1>c2)//do not understand proper
         return true;
         else
         return false;
