6c125da3da85b88b1f6d1857b1085b45a4c04237
2k24it12414004-cloud <2k24it12414004@gmail.com>
2026-09-09 11:54:12 +0530

    Time: 5 ms (24.62%), Space: 25.9 MB (9.87%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -22,20 +22,22 @@ void fun(TreeNode* node,vector<int>&ans){
         vector<int>ans;
         fun(root,ans);
         map<int,int>mp;//map
-        //indices,frequency
+        //element,frequency
         for(auto freq:ans){
             mp[freq]++;
         }
         int mini=INT_MIN;
         int index=0;
+        //isse loop ma max frequency nikala
         for(auto it:mp){
             int p=it.second;//
             if(p>mini){
                 mini=p;
-                index=it.first;
+              //  index=it.first;
             }
         }
         vector<int>res;
+        //isse loop har element frequency=maxfrequency
         for(auto it:mp){
             if(it.second==mini){
                res.push_back(it.first); 
