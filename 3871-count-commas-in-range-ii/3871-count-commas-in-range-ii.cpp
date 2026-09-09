e15d7f2023d090bed9b1c23deafc1793b3d1dd10
2k24it12414004-cloud <2k24it12414004@gmail.com>
2026-09-09 18:02:25 +0530

    Time: 0 ms (100%), Space: 9 MB (57.33%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,7 +3,7 @@ public:
     long long countCommas(long long n) {
         long long ans=0;
         long long base=1000;
-        for(int i=1;i<6;i++){
+        for(int i=0;i<5;i++){
      ans=ans+max(0LL,n-base+1);
      base*=1000;
         }
