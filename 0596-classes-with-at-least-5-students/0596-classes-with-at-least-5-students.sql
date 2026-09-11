b84bfce58d17d3bc54d08e91461cb7d8120147d1
2k24it12414004-cloud <2k24it12414004@gmail.com>
2026-09-11 19:17:09 +0530

    Time: 363 ms (48.81%), Space: 0B (100%) - LeetHub

---
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with students_per_class as
+(
+    select class
+,count(student) as students_count
+    from Courses
+    group by class
+)select class
+    from students_per_class
+    where students_count>=5
