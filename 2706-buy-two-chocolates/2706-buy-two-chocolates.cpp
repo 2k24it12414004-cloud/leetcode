7441be10565a032b1b208282fe41659dcbeade6f
2k24it12414004-cloud <2k24it12414004@gmail.com>
2026-09-25 16:46:11 +0530

    Time: 3 ms (14.06%), Space: 52.7 MB (2.71%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -3,10 +3,19 @@ public:
 //bekar approach find min 1 ,min 2 then subtract money se
 //if sum bade hoga ,money se so return money
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(),prices.end());
+        // sort(prices.begin(),prices.end());
+        // int n=prices.size();
+        // int min1=prices[0];
+        // int min2=prices[1];
+        priority_queue<int,vector<int>,greater<int>>pq;
         int n=prices.size();
-        int min1=prices[0];
-        int min2=prices[1];
+        for(int i=0;i<n;i++){
+            pq.push(prices[i]);
+        }
+        int min1=pq.top();
+        pq.pop();
+        int min2=pq.top();
+        pq.pop();
         if(min1+min2>money)
         return money;
         if(min1+min2==money)
